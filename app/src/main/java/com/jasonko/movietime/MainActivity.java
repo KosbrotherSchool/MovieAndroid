f82ef1acdbf1e1e92b874b21d0575d5ac7722e37
larryq0123 <larryq0123@gmail.com>
2015-08-20 15:58:38 +0800

    add toolbar

--- app/src/main/java/com/jasonko/movietime/MainActivity.java
+++ app/src/main/java/com/jasonko/movietime/MainActivity.java
@@ -1,27 +1,39 @@
 package com.jasonko.movietime;
 
 import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.quinny898.library.persistentsearch.SearchBox;
+import com.quinny898.library.persistentsearch.SearchResult;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
     private Button btn_theaters;
+    private SearchBox searchBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        searchBox = (SearchBox) findViewById(R.id.searchbox);
         btn_theaters = (Button) findViewById(R.id.btn_theaters);
-
-
-
+        setSearchBar();
 
 
         //打開戲院的activity
@@ -35,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+
 
     //test
     @Override
@@ -58,5 +72,68 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+    private void setSearchBar(){
+        searchBox.enableVoiceRecognition(this);
+        searchBox = (SearchBox) findViewById(R.id.searchbox);
+        for(int x = 0; x < 10; x++){
+            SearchResult option = new SearchResult("Result " + Integer.toString(x), getResources().getDrawable(R.drawable.ic_action_mic));
+            searchBox.addSearchable(option);
+        }
+        searchBox.setLogoText("My App");
+        searchBox.setMenuListener(new SearchBox.MenuListener(){
+
+            @Override
+            public void onMenuClick() {
+                //Hamburger has been clicked
+                Toast.makeText(MainActivity.this, "Menu click", Toast.LENGTH_LONG).show();
+            }
+
+        });
+        searchBox.setSearchListener(new SearchBox.SearchListener() {
+
+            @Override
+            public void onSearchOpened() {
+                //Use this to tint the screen
+            }
+
+            @Override
+            public void onSearchClosed() {
+                //Use this to un-tint the screen
+            }
+
+            @Override
+            public void onSearchTermChanged() {
+                //React to the search term changing
+                //Called after it has updated results
+            }
+
+            @Override
+            public void onSearch(String searchTerm) {
+                Toast.makeText(MainActivity.this, searchTerm + " Searched", Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onSearchCleared() {
+
+            }
+
+        });
+    }
+
+
+    //需要處理語音搜尋時，再回來coding
+    /*@Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (isAdded() && requestCode == SearchBox.VOICE_RECOGNITION_CODE && resultCode == getActivity().RESULT_OK) {
+            ArrayList<String> matches = data
+                    .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            searchBox.populateEditText(matches);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }*/
 
 }
